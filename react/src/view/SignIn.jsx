13010f2e3546be9b6f4e2925f9f37ed7b005f0cd
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-08-04 23:59:32 +0800

    added sign-in form

--- react/src/view/SignIn.jsx
+++ react/src/view/SignIn.jsx
@@ -1,14 +1,50 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import axios from "axios"
 
 export default function SignIn(){
-  const [email,setEmail] = useState();
+  const [email,setEmail] = useState('');
+  const [users,setUser] = useState([]);
+  const [error,setError] = useState(null);
+  const [isLoading,setLoading] = useState(null);
+  
+
   const handleSubmit = (e) =>{
     e.preventDefault();
   }
+
+  useEffect(() => {
+    axios.post('https://jsonplaceholder.typicode.com/posts', email)
+    .then(response => {
+      console.log('Post created:', response.data);
+      // Reset form fields
+      console.log(email)
+    })
+    .catch(error => {
+      console.error('Error creating post:', error);
+    });
+  } )
+ /* useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts//')
+    .then(response => {
+        setUser(response.data)
+        setLoading(true)
+    })
+    .catch(err => {
+      setError(err.message);
+      setLoading(false)
+    })
+  },[])*/
+  
   return (
     <>
+     <ul>
+       {isLoading ?  users.map(item => (
+            <li key={item.id}>{item.id}. {item.title}</li>
+          )) : error}
+      </ul>
+      
       <form onSubmit={handleSubmit}>
-        <p>{email}</p>
+        <input type="text" name="email" placeholder="Email" onChange={e => setEmail(e.target.value)}/>
         <input type="text" name="email" placeholder="Email" onChange={e => setEmail(e.target.value)}/>
       </form>
     </>
